4a77b978591c6e2df5eecbc88301ff2374a1fe77
ArielWu0203 <lucky020372@gmail.com>
2020-03-25 14:54:28 +0800

    Add page2 header and button, modify slider animation

--- first.js
+++ first.js
@@ -1,11 +1,12 @@
 var count=1;
 $('.right').click(function() {
     if(count==1) {
-        document.getElementById("second").style.left="100%";
+        document.getElementById("second").style.left="+100%";
         $('#first').animate({'right': "+=100%"},1000);
         $('#title').animate({'right': "+=100%"},1000);
         $('#second').animate({'left': "-=100%"},1000,function(){
             $('#title').css("display","none");
+            $('#title2').css("display","block");
         }); 
         count=2;
     }
@@ -15,7 +16,13 @@ $('.left').click(function() {
     if(count==2) {
         $('#first').animate({'right': "-=100%"},1000);
         $('#title').animate({'right': "-=100%"},1000);
-        $('#title').css("display","block");
+        $('#title2').animate({'left': "+=100%"},1000);
+        $('#second').animate({'left': "+=100%"},1000,function(){
+            document.getElementById("second").style.left="-100%";
+            $('#title').css("display","block");
+            $('#title2').css("display","none");
+            $('#title2').animate({'left': "-=100%"},1000);
+        });
         count=1;
     }
 })
