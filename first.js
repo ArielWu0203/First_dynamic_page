a1503bdc70d3ff2c4fbd3a95793ba434e8a5fe59
ArielWu0203 <lucky020372@gmail.com>
2020-03-26 13:04:58 +0800

    Slider fixed

--- first.js
+++ first.js
@@ -1,27 +1,28 @@
 var count=1;
 $('.right').click(function() {
     if(count==1) {
-        document.getElementById("second").style.left="+100%";
-        $('#first').animate({'right': "+=100%"},1000);
-        $('#title').animate({'right': "+=100%"},1000);
-        $('#second').animate({'left': "-=100%"},1000,function(){
-            $('#title').css("display","none");
-            $('#title2').css("display","block");
-        }); 
+        $('#second').animate({'left': "100%"},10,function() {
+            $('#first').animate({'right': "100%"},1000);
+            $('#title').animate({'right': "100%"},1000);
+            $('#second').animate({'left': "0%"},1000,function(){
+                $('#title').css("display","none");
+                $('#title2').css("display","block");
+            }); 
+        })
         count=2;
     }
 })
 
 $('.left').click(function() {
     if(count==2) {
-        $('#first').animate({'right': "-=100%"},1000);
-        $('#title').animate({'right': "-=100%"},1000);
-        $('#title2').animate({'left': "+=100%"},1000);
-        $('#second').animate({'left': "+=100%"},1000,function(){
-            document.getElementById("second").style.left="-100%";
+        $('#first').animate({'right': "0%"},1000);
+        $('#title').animate({'right': "0%"},1000);
+        $('#title2').animate({'left': "100%"},1000);
+        $('#second').animate({'left': "100%"},1000,function(){
+            document.getElementById("second").style.left="0%";
+            document.getElementById("title2").style.left="0%";
             $('#title').css("display","block");
             $('#title2').css("display","none");
-            $('#title2').animate({'left': "-=100%"},1000);
         });
         count=1;
     }
